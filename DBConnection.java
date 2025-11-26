51652c3c9b1eaf062773c207c0a4bd348f038e56
vennapusavasantha57 <vennapusavasantha6@gmail.com>
2025-11-26 11:40:22 +0530

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,23 @@
+package com.vasantha.bank.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/bank_db?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "your_mysql_password";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
